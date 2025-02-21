e16b61399df67a7a22b4c5518ce50eaadf965956
SandorMarko1 <beamarko08@gmail.com>
2025-02-21 10:17:30 +0100

    classal és táblával

--- WinForms_orszagok/Form1.Designer.cs
+++ WinForms_orszagok/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.classos_feltoltes = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tabla1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla1)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
@@ -58,7 +61,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(187, 210);
+            this.checkBox1.Location = new System.Drawing.Point(29, 243);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(134, 17);
             this.checkBox1.TabIndex = 2;
@@ -68,7 +71,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(187, 233);
+            this.textBox1.Location = new System.Drawing.Point(29, 266);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(170, 20);
             this.textBox1.TabIndex = 3;
@@ -80,6 +83,7 @@
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(134, 173);
             this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // classos_feltoltes
             // 
@@ -89,12 +93,37 @@
             this.classos_feltoltes.TabIndex = 5;
             this.classos_feltoltes.Text = "feltöltés";
             this.classos_feltoltes.UseVisualStyleBackColor = true;
+            this.classos_feltoltes.Click += new System.EventHandler(this.classos_feltoltes_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(722, 220);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(134, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Kiválasztás elfogadása";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // tabla1
+            // 
+            this.tabla1.AllowUserToAddRows = false;
+            this.tabla1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tabla1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla1.Location = new System.Drawing.Point(428, 266);
+            this.tabla1.Name = "tabla1";
+            this.tabla1.Size = new System.Drawing.Size(439, 70);
+            this.tabla1.TabIndex = 8;
+            this.tabla1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabla1_CellContentClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(879, 487);
+            this.Controls.Add(this.tabla1);
+            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.classos_feltoltes);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.textBox1);
@@ -103,6 +132,7 @@
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tabla1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +146,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Button classos_feltoltes;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.DataGridView tabla1;
     }
 }
 
--- WinForms_orszagok/Form1.cs
+++ WinForms_orszagok/Form1.cs
@@ -18,38 +18,38 @@ namespace WinForms_orszagok
             InitializeComponent();
         }
 
+            public List<string[]> orszagok = new List<string[]>();
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-                List<string[]> orszagok = new List<string[]>();
 
-                using (StreamReader sr = new StreamReader("forras_azsia.txt"))
+            using (StreamReader sr = new StreamReader("forras_azsia.txt"))
+            {
+                while (!sr.EndOfStream)
                 {
-                    while (!sr.EndOfStream)
-                    {
-                        orszagok.Add(sr.ReadLine().Split(';'));
-                    }
+                    orszagok.Add(sr.ReadLine().Split(';'));
                 }
+            }
 
-                if (checkBox1.Checked)
+            if (checkBox1.Checked)
+            {
+                if (listBox1.SelectedItem != null)
                 {
-                    if (listBox1.SelectedItem != null)
-                    {
-                        string selectedItem = listBox1.SelectedItem.ToString();
+                    string selectedItem = listBox1.SelectedItem.ToString();
 
-                        for (int i = 0; i < listBox1.Items.Count; i++)
+                    for (int i = 0; i < listBox1.Items.Count; i++)
+                    {
+                        if (listBox1.Items[i].ToString() == selectedItem)
                         {
-                            if (listBox1.Items[i].ToString() == selectedItem)
+                            textBox1.Clear();
+                            foreach (var item in orszagok[i])
                             {
-                                textBox1.Clear();
-                                foreach (var item in orszagok[i])
-                                {
-                                    textBox1.AppendText(item + "; ");
-                                }
-                                break;
+                                textBox1.AppendText(item + "; ");
                             }
+                            break;
                         }
                     }
                 }
+            }
         }
 
         public void feltolt_gomb_Click(object sender, EventArgs e)
@@ -71,10 +71,114 @@ namespace WinForms_orszagok
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            if (checkBox1.Checked)
+            {
+                textBox1.Clear();
+                foreach (var item in orszagok[listBox1.SelectedIndex])
+                {
+                    textBox1.AppendText(item + "; ");
+                }
+            }
+            else
+            {
+                textBox1.Clear();
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null && checkBox2.Checked)
+            {
+                string kivalasztottOrszag = listBox2.SelectedItem.ToString();
+                MegjelenitAdatokat(kivalasztottOrszag);
+            }
+        }
+
+        private void classos_feltoltes_Click(object sender, EventArgs e)
+        {
+            List<Europai> lista = new List<Europai>();
+            var importaltOrszagok = beolvas.beolvasas("forras_europa.txt");
+            lista.AddRange(importaltOrszagok);
+            for (int i = 0; i < lista.Count; i++)
+            {
+                listBox2.Items.Add(lista[i].orszag);
+            }
+        }
+
+        private void tabla_Paint(object sender, PaintEventArgs e)
+        {
 
         }
 
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null && checkBox2.Checked)
+            {
+                string kivalasztottOrszag = listBox2.SelectedItem.ToString();
+                MegjelenitAdatokat(kivalasztottOrszag);
+            }
+        }
+        private void MegjelenitAdatokat(string orszag)
+        {
+            Europai kivalasztott = beolvas.beolvasas("forras_europa.txt")
+                .FirstOrDefault(o => o.orszag == orszag);
 
+            if (kivalasztott == null) return;
 
+            tabla1.Rows.Clear();
+            tabla1.Columns.Clear();
+
+            tabla1.ColumnCount = 5;
+            tabla1.Columns[0].Name = "Ország";
+            tabla1.Columns[1].Name = "Főváros";
+            tabla1.Columns[2].Name = "Terület";
+            tabla1.Columns[3].Name = "Népesség";
+            tabla1.Columns[4].Name = "Népsűrűség";
+
+            tabla1.Rows.Add(kivalasztott.orszag, kivalasztott.fovaros, kivalasztott.terulet, kivalasztott.nepesseg, kivalasztott.nepsuruseg);
+        }
+
+        private void tabla1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
+
+    class Europai
+    {
+        public string orszag { get; set; }
+        public string fovaros { get; set; }
+        public int terulet { get; set; }
+        public int nepesseg { get; set; }
+        public int nepsuruseg { get; set; }
+
+        public Europai(string orszag, string fovaros, int terulet, int nepesseg, int nepsuruseg){
+            this.orszag = orszag;
+            this.fovaros = fovaros;
+            this.terulet = terulet;
+            this.nepesseg = nepesseg;
+            this.nepsuruseg = nepsuruseg;
+        }
+    }
+
+    class beolvas 
+    {
+        public static List<Europai> beolvasas(string file)
+        {
+            List<Europai> orszagok = new List<Europai>();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    if (sor.Length == 5 && int.TryParse(sor[2], out int terulet) && int.TryParse(sor[3], out int nepesseg) && int.TryParse(sor[4], out int nepsuruseg))
+                    {
+                        orszagok.Add(new Europai(sor[0], sor[1], terulet, nepesseg, nepsuruseg));
+                    }
+                }
+            }
+            return orszagok;
+        }
+    }
+
 }
