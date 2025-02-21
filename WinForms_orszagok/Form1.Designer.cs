cd3379d918b0a6433f3e313f2a589060e8aed33b
SandorMarko1 <beamarko08@gmail.com>
2025-02-21 08:44:09 +0100

    Ázsiai országok - class nélkül

--- WinForms_orszagok/Form1.Designer.cs
+++ WinForms_orszagok/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.feltolt_gomb = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.classos_feltoltes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(134, 186);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // feltolt_gomb
+            // 
+            this.feltolt_gomb.Location = new System.Drawing.Point(29, 205);
+            this.feltolt_gomb.Name = "feltolt_gomb";
+            this.feltolt_gomb.Size = new System.Drawing.Size(134, 23);
+            this.feltolt_gomb.TabIndex = 1;
+            this.feltolt_gomb.Text = "feltöltés";
+            this.feltolt_gomb.UseVisualStyleBackColor = true;
+            this.feltolt_gomb.Click += new System.EventHandler(this.feltolt_gomb_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(187, 210);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Kiválasztás elfogadása";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 233);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(722, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(134, 173);
+            this.listBox2.TabIndex = 4;
+            // 
+            // classos_feltoltes
+            // 
+            this.classos_feltoltes.Location = new System.Drawing.Point(722, 191);
+            this.classos_feltoltes.Name = "classos_feltoltes";
+            this.classos_feltoltes.Size = new System.Drawing.Size(134, 23);
+            this.classos_feltoltes.TabIndex = 5;
+            this.classos_feltoltes.Text = "feltöltés";
+            this.classos_feltoltes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(879, 487);
+            this.Controls.Add(this.classos_feltoltes);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.feltolt_gomb);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button feltolt_gomb;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button classos_feltoltes;
     }
 }
 
--- WinForms_orszagok/Form1.cs
+++ WinForms_orszagok/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WinForms_orszagok
 {
@@ -16,5 +17,64 @@ namespace WinForms_orszagok
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                List<string[]> orszagok = new List<string[]>();
+
+                using (StreamReader sr = new StreamReader("forras_azsia.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        orszagok.Add(sr.ReadLine().Split(';'));
+                    }
+                }
+
+                if (checkBox1.Checked)
+                {
+                    if (listBox1.SelectedItem != null)
+                    {
+                        string selectedItem = listBox1.SelectedItem.ToString();
+
+                        for (int i = 0; i < listBox1.Items.Count; i++)
+                        {
+                            if (listBox1.Items[i].ToString() == selectedItem)
+                            {
+                                textBox1.Clear();
+                                foreach (var item in orszagok[i])
+                                {
+                                    textBox1.AppendText(item + "; ");
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+        }
+
+        public void feltolt_gomb_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            List<string[]> orszagok = new List<string[]>();
+            using (StreamReader sr = new StreamReader("forras_azsia.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    orszagok.Add(sr.ReadLine().Split(';'));
+                }
+            }
+            for (int i = 0; i < orszagok.Count; i++)
+            {
+                listBox1.Items.Add(orszagok[i][0]);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
